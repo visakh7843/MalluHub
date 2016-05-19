7799070cb8e96c21be2ddff12c6e1180e82e635c
Dany <dannyleduc16@hotmail.com>
2016-05-19 16:26:14 +0200

    Added pages and contact form

--- app.js
+++ app.js
@@ -5,11 +5,50 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname,'public')));
 app.get('/', function(req, res){
-	res.send('<h1>Hello World</h1>');
+	res.render('index',{title:'Welcome'});
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+app.get('/contact', function(req, res){
+	res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'mail',
+			pass: 'pwd'
+		}
+	});
+	var mailOptions = {
+		from : 'Dan <aaa@gmail.com',
+		to : 'bbb@gmail.com',
+		subject : 'Website submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+' Email : '+req.body.email+ ' Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions,function(error,info){
+		if(error)
+		{
+			console.log(error);
+			res.redirect('/');
+		}else{
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
 });
 
 app.listen(3000);
