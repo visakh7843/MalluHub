e952cde5483708d730c0bec8c222929221819854
visakh7843 <vishakajith@gmail.com>
2017-07-08 22:08:59 +0530

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/', function(req, res){
-	res.render('index',{title:'Welcome'});
+	res.render('index');
 });
 
 app.get('/about', function(req, res){
@@ -22,7 +22,39 @@ app.get('/about', function(req, res){
 app.get('/contact', function(req, res){
 	res.render('contact');
 });
-
+app.get('/temp1', function(req, res){
+	res.render('temp1');
+});
+app.get('/temp2', function(req, res){
+	res.render('temp2');
+});
+app.get('/temp3', function(req, res){
+	res.render('temp3');
+});
+app.get('/temp4', function(req, res){
+	res.render('temp4');
+});
+app.get('/temp5', function(req, res){
+	res.render('temp5');
+});
+app.get('/temp6', function(req, res){
+	res.render('temp6');
+});
+app.get('/temp7', function(req, res){
+	res.render('temp7');
+});
+app.get('/temp8', function(req, res){
+	res.render('temp8');
+});
+app.get('/temp9', function(req, res){
+	res.render('temp9');
+});
+app.get('/temp10', function(req, res){
+	res.render('temp10');
+});
+app.get('/temp11', function(req, res){
+	res.render('temp11');
+});
 app.post('/contact/send', function(req, res){
 	var transporter = nodemailer.createTransport({
 		service: 'Gmail',
